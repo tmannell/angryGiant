504879e82994c5b6ed58830b8a82c64054d0817c
tmannell <tmannell@rnao.ca>
2017-04-10 15:32:07 -0400

    First commit

---
+++ web/app/controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+class Controller {
+
+  public function beforeroute() {
+
+  }
+
+  public function afterroute() {
+
+  }
+}
---
+++ web/app/controllers/MainController.php
@@ -0,0 +1,8 @@
+<?php
+
+class MainController extends Controller {
+
+  function home($f3) {
+    echo 'Welcome to Angry Giant!';
+  }
+}
