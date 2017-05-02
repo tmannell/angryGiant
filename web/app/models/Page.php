73b98b30fdb2aa997a6f2644e33e0c93224f2571
thomasmannell <tmannell@gmail.com>
2017-05-02 07:29:45 -0400

    Added a model for the Page obj

---
+++ web/app/models/Page.php
@@ -0,0 +1,10 @@
+<?php
+
+\Base::instance()->set('DB',new DB\SQL(\Base::instance()->get('sqliteDB')));
+
+class Page extends \DB\SQL\Mapper {
+
+  public function __construct() {
+    parent::__construct( \Base::instance()->get('DB'), 'pages');
+  }
+}
