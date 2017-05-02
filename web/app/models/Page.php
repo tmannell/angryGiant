4a45d339c877731558f997c22311dd3ec8d8c018
tmannell <tmannell@rnao.ca>
2017-05-02 15:49:54 -0400

    updated Page model

--- web/app/models/Page.php
+++ web/app/models/Page.php
@@ -7,4 +7,8 @@ class Page extends \DB\SQL\Mapper {
   public function __construct() {
     parent::__construct( \Base::instance()->get('DB'), 'pages');
   }
+
+  public function listPageNumbersInStory($sid) {
+    return $this->select('page_number', ['story_id = ?', $sid], ['order' => 'page_number ASC']);
+  }
 }
