8028397b8317e72e0342b0c6bec4fa248baaeb6f
tmannell <tmannell@rnao.ca>
2017-05-05 16:03:48 -0400

    Added a list all users function

--- web/app/models/User.php
+++ web/app/models/User.php
@@ -14,4 +14,15 @@ class User extends \DB\SQL\Mapper {
   public function __construct() {
     parent::__construct( \Base::instance()->get('DB'), 'users');
   }
+
+  /**
+   * Returns all users.
+   *
+   * @param $order
+   *  Sort order must be string with '<field_name> (asc|desc)'
+   * @return object
+   */
+  public function listAllUsers($order) {
+    return $this->select('*', null, ['order' => implode(' ', $order)]);
+  }
 }
